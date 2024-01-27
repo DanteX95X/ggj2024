8e3fa4a2b70cafcdf5a26f105adc83f963808c64
dante <daniellewinski95@gmail.com>
2024-01-27 11:16:43 +0100

    Implemented uniformly accelerated fall.

--- source/game_logic/game_manager.cpp
+++ source/game_logic/game_manager.cpp
@@ -1,6 +1,7 @@
 #include "game_manager.h"
 
 #include <godot_cpp/classes/engine.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
 
 
 namespace ggj
@@ -29,6 +30,22 @@ void GameManager::_ready()
 	activeBlock->set_position(godot::Vector2{LEFT_BOUNDS, 100});
 	activeBlock->SetShape(SHAPES.back(), NODE_SIZE);
 	add_child(activeBlock);
+}
+
+void GameManager::_physics_process(double delta)
+{
+	auto velocity = previousVelocity + ACCELERATION * delta;
+	auto displacement = velocity * delta;
+	accumulatedDistance += displacement;
+	godot::UtilityFunctions::print("accumulated distance", accumulatedDistance);
+
+	previousVelocity = velocity;
+
+	if(accumulatedDistance > 1)
+	{
+		accumulatedDistance -= 1;
+		activeBlock->translate(godot::Vector2{0, 1} * NODE_SIZE);
+	}
 }
 
 godot::Ref<godot::PackedScene> GameManager::GetBlockScene() const
--- source/game_logic/game_manager.h
+++ source/game_logic/game_manager.h
@@ -20,6 +20,7 @@ public:
 	GameManager();
 
 	void _ready() override;
+	void _physics_process(double delta) override;
 
 	godot::Ref<godot::PackedScene> GetBlockScene() const;
 	void SetBlockScene(godot::Ref<godot::PackedScene> blockScene);
@@ -32,12 +33,16 @@ public:
 
 private:
 	Block* activeBlock = nullptr;
+	float previousVelocity = 0;
+	float accumulatedDistance = 0;
+
 
 	godot::Ref<godot::PackedScene> blockScene;
 
 	const float LEFT_BOUNDS = 100.0f;
 	const float NODE_SIZE = 64.0f;
 	const int WIDTH = 10;
+	const float ACCELERATION = 0.5f;
 
 	const std::vector<std::vector<godot::Vector2>> SHAPES =
 	{
