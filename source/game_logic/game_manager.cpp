26e6b3b21f95510501d746438a8609797299a76b
dante <daniellewinski95@gmail.com>
2024-01-27 12:19:22 +0100

    Spawning block at the given position. Implemented block snapping to grid when out of bounds.

--- source/game_logic/game_manager.cpp
+++ source/game_logic/game_manager.cpp
@@ -26,10 +26,8 @@ void GameManager::_ready()
 		return;
 	}
 
-	activeBlock = static_cast<Block*>(blockScene->instantiate());
-	activeBlock->set_position(godot::Vector2{LEFT_BOUNDS, 100});
-	activeBlock->SetShape(SHAPES.back(), NODE_SIZE);
-	add_child(activeBlock);
+	SpawnBlockAt(godot::Vector2i{1, 0}, SHAPES.back());
+	SnapBlockToGrid();
 }
 
 void GameManager::_physics_process(double delta)
@@ -42,8 +40,67 @@ void GameManager::_physics_process(double delta)
 	if(accumulatedDistance > 1)
 	{
 		accumulatedDistance -= 1;
-		activeBlock->translate(godot::Vector2{0, 1} * NODE_SIZE);
+//		activeBlock->translate(godot::Vector2{0, 1} * NODE_SIZE);
+//		blockGridPosition += godot::Vector2i{0, 1};
 	}
+}
+
+void GameManager::SpawnBlockAt(godot::Vector2i gridPosition, std::vector<godot::Vector2i> shape)
+{
+	activeBlock = static_cast<Block*>(blockScene->instantiate());
+	activeBlock->set_position(godot::Vector2{LEFT_BOUNDS + NODE_SIZE * gridPosition.x,
+	                                         TOP_BOUNDS + NODE_SIZE * gridPosition.y});
+	activeBlock->SetShape(shape, NODE_SIZE);
+	add_child(activeBlock);
+	blockGridPosition = gridPosition;
+}
+
+void GameManager::MoveBlockLeft()
+{
+	activeBlock->translate(godot::Vector2{-NODE_SIZE, 0});
+	blockGridPosition.x -= 1;
+	SnapBlockToGrid();
+}
+
+void GameManager::MoveBlockRight()
+{
+	activeBlock->translate(godot::Vector2{NODE_SIZE, 0});
+	blockGridPosition.x += 1;
+	SnapBlockToGrid();
+}
+
+void GameManager::RotateBlock()
+{
+	activeBlock->Rotate(NODE_SIZE);
+	SnapBlockToGrid();
+}
+
+void GameManager::SnapBlockToGrid()
+{
+	BlockSpan span = activeBlock->GetSpan();
+	BlockSpan spanOnGrid{};
+	spanOnGrid.left = blockGridPosition.x + span.left;
+	spanOnGrid.right = blockGridPosition.x + span.right;
+	spanOnGrid.top = blockGridPosition.y + span.top;
+	spanOnGrid.bottom = blockGridPosition.y + span.bottom;
+
+	if(spanOnGrid.left < 0)
+	{
+		blockGridPosition.x -= spanOnGrid.left;
+		const auto& position = activeBlock->get_position();
+		activeBlock->translate(godot::Vector2{-spanOnGrid.left * NODE_SIZE, 0});
+	}
+	else if(spanOnGrid.right >= GRID_WIDTH)
+	{
+		int displacement = spanOnGrid.right - GRID_WIDTH + 1;
+		blockGridPosition.x -= displacement;
+		activeBlock->translate(godot::Vector2{-displacement * NODE_SIZE, 0});
+	}
+}
+
+Block* GameManager::GetActiveBlock() const
+{
+	return activeBlock;
 }
 
 godot::Ref<godot::PackedScene> GameManager::GetBlockScene() const
@@ -54,26 +111,6 @@ godot::Ref<godot::PackedScene> GameManager::GetBlockScene() const
 void GameManager::SetBlockScene(godot::Ref<godot::PackedScene> blockScene)
 {
 	this->blockScene = blockScene;
-}
-
-void GameManager::MoveBlockLeft()
-{
-	activeBlock->translate(godot::Vector2{-NODE_SIZE, 0});
-}
-
-void GameManager::MoveBlockRight()
-{
-	activeBlock->translate(godot::Vector2{NODE_SIZE, 0});
-}
-
-void GameManager::RotateBlock()
-{
-	activeBlock->Rotate(NODE_SIZE);
-}
-
-Block* GameManager::GetActiveBlock() const
-{
-	return activeBlock;
 }
 
 }
--- source/game_logic/game_manager.h
+++ source/game_logic/game_manager.h
@@ -22,17 +22,21 @@ public:
 	void _ready() override;
 	void _physics_process(double delta) override;
 
-	godot::Ref<godot::PackedScene> GetBlockScene() const;
-	void SetBlockScene(godot::Ref<godot::PackedScene> blockScene);
+	void SpawnBlockAt(godot::Vector2i gridPosition, std::vector<godot::Vector2i> shape);
 
 	void MoveBlockLeft();
 	void MoveBlockRight();
 	void RotateBlock();
+	void SnapBlockToGrid();
 
 	Block* GetActiveBlock() const;
 
+	godot::Ref<godot::PackedScene> GetBlockScene() const;
+	void SetBlockScene(godot::Ref<godot::PackedScene> blockScene);
+
 private:
 	Block* activeBlock = nullptr;
+	godot::Vector2i blockGridPosition{0, 0};
 	float previousVelocity = 0;
 	float accumulatedDistance = 0;
 
@@ -40,6 +44,7 @@ private:
 	godot::Ref<godot::PackedScene> blockScene;
 
 	const float LEFT_BOUNDS = 100.0f;
+	const float TOP_BOUNDS = 100.0f;
 	const float NODE_SIZE = 64.0f;
 	const float ACCELERATION = 0.5f;
 
