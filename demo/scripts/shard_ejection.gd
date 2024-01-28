18778d290b1ac06e142744692887ccd9ee4f0b9b
dante <daniellewinski95@gmail.com>
2024-01-28 12:03:48 +0100

    Jar shattering animation.

---
+++ demo/scripts/shard_ejection.gd
@@ -0,0 +1,16 @@
+extends Node2D
+class_name ShardEjection
+
+@export var shardScene: PackedScene
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	get_parent().get_node("GameManager").connect("game_over", eject_shards)
+
+
+func eject_shards(message):
+	print("ufo")
+	for n in 100:
+		var shard: RigidBody2D = shardScene.instantiate()
+		add_child(shard)
+		shard.apply_impulse(Vector2(rng.randf_range(-1.0, 1.0), rng.randf_range(-1.0, 1.0)).normalized() * 1000)
